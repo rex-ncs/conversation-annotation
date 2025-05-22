6e11c11ff13b3d61b9113eb36134f72df2b416e9
rexong <rexonghanwei200035@gmail.com>
2025-05-23 05:13:26 +0800

    chore: setup shadcn

---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+// tailwind.config.js
+const { fontFamily } = require("tailwindcss/defaultTheme");
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: ["class"],
+  content: [
+    "./app/**/*.{ts,tsx}", 
+    "./components/**/*.{ts,tsx}",
+  ],
+  theme: {
+    container: {
+      center: true,
+      padding: "2rem",
+      screens: {
+        "2xl": "1400px",
+      },
+    },
+    extend: {
+      fontFamily: {
+        sans: ["var(--font-sans)", ...fontFamily.sans],
+      },
+    },
+  },
+  plugins: [require("tailwindcss-animate")],
+};
