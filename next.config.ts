459733c6ae5d37026a1fdc37f4f6399186258f1b
rexong <rexonghanwei200035@gmail.com>
2025-05-26 21:42:35 +0800

    feat: basePath and assetPrefix on Prod

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
+const isProd = process.env.ENVIRONMENT === "production";
+
 const nextConfig: NextConfig = {
-  /* config options here */
+  assetPrefix: isProd ? "/annotate" : "",
+  basePath: isProd ? "/annotate" : "",
 };
 
 export default nextConfig;
