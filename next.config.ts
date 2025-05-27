140c62eb067ef8ef40bf9e4cdce8161a5c25695c
rexong <rexonghanwei200035@gmail.com>
2025-05-27 22:23:52 +0800

    chore: change env var

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,6 @@
 import type { NextConfig } from "next";
 
-const isProd = process.env.ENVIRONMENT === "production";
+const isProd = process.env.NODE_ENV === "production";
 
 const nextConfig: NextConfig = {
   assetPrefix: isProd ? "/annotate" : "",
