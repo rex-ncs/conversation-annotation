483c1a5721ad9e536f727b65003d5c0ba8f1645e
rexong <rexonghanwei200035@gmail.com>
2025-06-03 19:37:27 +0800

    feat: update navbar for admin

--- components/header.tsx
+++ components/header.tsx
@@ -5,14 +5,14 @@ import { logout } from "@/app/actions/auth"
 import NavBar from "./nav-bar"
 import { UserCircle } from "lucide-react"
 
-export default function Header( { username }: { username: string }) {
+export default function Header( { username, isAdmin }: { username: string, isAdmin: boolean }) {
 
     return (
       <header className="bg-white shadow">
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-4 flex justify-between items-center">
           <div className="flex items-center gap-6">
             <h1 className="text-xl font-semibold">Conversation Annotation Tool</h1>
-            <NavBar />
+            <NavBar isAdmin={isAdmin} />
           </div>
           <div className="flex items-center gap-4">
             <div className="flex items-center gap-2">
